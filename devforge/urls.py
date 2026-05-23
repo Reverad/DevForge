7baabcec7fc592ed8790ee8bd2626e3ab3d2f0a3
Reverad <sektumpro@gmail.com>
2026-05-23 19:47:38 +0300

    add registration

--- devforge/urls.py
+++ devforge/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
-    # path("user/", include("users.urls", namespace="users")),
+    path("user/", include("users.urls", namespace="users")),
     # path("team/", include("teams.urls", namespace="teams")),
     # path("project/", include("projects.urls", namespace="projects")),
     # path("task/", include("tasks.urls", namespace="tasks")),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from users.forms import RegisterForm
+
+
+User = get_user_model()
+
+class RegisterView(generic.CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy('core:index')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
