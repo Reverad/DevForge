e11f1719a4e48ae6dae58fd7c07d59f9c8aca5a2
Reverad <sektumpro@gmail.com>
2026-05-26 23:55:35 +0300

    refactor TeamsDetailView into separate views

--- teams/views.py
+++ teams/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
 
 from teams.forms import TeamCreateForm
@@ -41,47 +42,103 @@ class TeamCreateView(LoginRequiredMixin, generic.CreateView):
 class TeamDetailView(LoginRequiredMixin, generic.DetailView):
     model = Team
 
-    def post(self, request, *args, **kwargs):
-        self.object: Team = self.get_object()
-        team = self.object
+
+class TeamDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Team
+    success_url = reverse_lazy("teams:team-list")
+
+    def dispatch(self, request, *args, **kwargs):
+        team = self.get_object()
 
         if team.owner != request.user:
             messages.error(request, "You do not have permission to perform this action.")
             return redirect(team.get_absolute_url())
 
-        if "delete_team" in request.POST:
-            team_name = team.name
-            team.delete()
-            messages.success(request, f"Team \"{team_name}\" has been successfully deleted.")
-            return redirect("teams:team-list")
+        return super().dispatch(request, *args, **kwargs)
 
-        elif "username" in request.POST:
-            username = request.POST.get("username").strip()
-            try:
-                user_to_add = User.objects.get(username=username)
+    def delete(self, request, *args, **kwargs):
+        team = self.get_object()
+        team_name = team.name
 
-                if user_to_add in team.members.all():
-                    messages.warning(request, f"User @{username} is already a member of this team.")
-                else:
-                    team.members.add(user_to_add)
-                    messages.success(request, f"User @{username} has been successfully added!")
-            except User.DoesNotExist:
-                messages.error(request, f"User @{username} not found.")
+        messages.success(
+            request,
+            f'Team "{team_name}" has been successfully deleted.'
+        )
 
-        elif "member_id" in request.POST:
-            member_id = request.POST.get("member_id")
-            user_to_kick = get_object_or_404(User, id=member_id)
+        return super().delete(request, *args, **kwargs)
 
-            if user_to_kick == team.owner:
-                messages.error(request, "You cannot kick the team owner.")
+
+class TeamAddMemberView(LoginRequiredMixin, generic.View):
+    def post(self, request, *args, **kwargs):
+        team = get_object_or_404(Team, pk=kwargs["pk"])
+
+        if team.owner != request.user:
+            messages.error(request, "You do not have permission to perform this action.")
+            return redirect(team.get_absolute_url())
+
+        username = request.POST.get("username", "").strip()
+
+        try:
+            user_to_add = User.objects.get(username=username)
+
+            if user_to_add in team.members.all():
+                messages.warning(
+                    request,
+                    f"User @{username} is already a member of this team."
+                )
             else:
-                team.members.remove(user_to_kick)
-                messages.success(request, f"User @{user_to_kick.username} has been removed from the team.")
+                team.members.add(user_to_add)
+                messages.success(
+                    request,
+                    f"User @{username} has been successfully added!"
+                )
 
-        elif "description" in request.POST:
-            new_description = request.POST.get("description").strip()
-            team.description = new_description
-            team.save()
-            messages.success(request, "Team description has been successfully updated.")
+        except User.DoesNotExist:
+            messages.error(request, f"User @{username} not found.")
+
+        return redirect(team.get_absolute_url())
+
+
+class TeamRemoveMemberView(LoginRequiredMixin, generic.View):
+    def post(self, request, *args, **kwargs):
+        team = get_object_or_404(Team, pk=kwargs["pk"])
+
+        if team.owner != request.user:
+            messages.error(request, "You do not have permission to perform this action.")
+            return redirect(team.get_absolute_url())
+
+        user_to_kick = get_object_or_404(
+            User,
+            pk=request.POST.get("member_id")
+        )
+
+        if user_to_kick == team.owner:
+            messages.error(request, "You cannot kick the team owner.")
+        else:
+            team.members.remove(user_to_kick)
+            messages.success(
+                request,
+                f"User @{user_to_kick.username} has been removed from the team."
+            )
+
+        return redirect(team.get_absolute_url())
+
+
+class TeamUpdateDescriptionView(LoginRequiredMixin, generic.View):
+
+    def post(self, request, *args, **kwargs):
+        team = get_object_or_404(Team, pk=kwargs["pk"])
+
+        if team.owner != request.user:
+            messages.error(request, "You do not have permission to perform this action.")
+            return redirect(team.get_absolute_url())
+
+        team.description = request.POST.get("description", "").strip()
+        team.save()
+
+        messages.success(
+            request,
+            "Team description has been successfully updated."
+        )
 
         return redirect(team.get_absolute_url())
