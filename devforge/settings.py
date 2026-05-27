b64dd69133840060a7bc3db9cd0343aa1e5fb1af
Reverad <sektumpro@gmail.com>
2026-05-27 12:43:49 +0300

    update settings.py

--- devforge/settings.py
+++ devforge/settings.py
@@ -9,17 +9,17 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-&9rf2(-38ef-ajzml%e5s7avdvq1!)979cf%f^f8t8-ftv!18%"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-&9rf2(-38ef-ajzml%e5s7avdvq1!)979cf%f^f8t8-ftv!18%")
 
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
