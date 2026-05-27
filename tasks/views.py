3df83defd255cced181e17423bacc4414ec28059
Reverad <sektumpro@gmail.com>
2026-05-27 15:45:18 +0300

    prepare project for deploy

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views import generic
 
 from projects.models import Project
